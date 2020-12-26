aa69727736d8c1cf75d7f1684c84090877531aba
orlandopadillad <71845994+orlandopadillad@users.noreply.github.com>
2020-12-25 19:15:20 -0500

    .

--- assets/js/json-manipulation.js
+++ assets/js/json-manipulation.js
@@ -1,5 +1,5 @@
 //json configuration for the new autocomplete searchbox
-const searchingg = document.getElementById('search'); //the searchbox...orlandop
+const search = document.getElementById('search'); //the searchbox...orlandop
 const matchList = document.getElementById('match-list');//the result of the search...orlandop
 
 
@@ -14,7 +14,7 @@ const matchList = document.getElementById('match-list');//the result of the sear
            return state.name.match(regex) || state.abbr.match(regex); //return the values of the json on the location state.name or the value on the location state.abbr.
          });
          
-            if (searchText.length === 0){ //this clean the data if the values on the search box is empty
+            if (searchText.length === 0) { //this clean the data if the values on the search box is empty
                matches = []; // this declare an emtpy array if the value of the search box is empty
                matchList.innerHTML = '';
             }
@@ -44,7 +44,7 @@ const matchList = document.getElementById('match-list');//the result of the sear
    //search the values.json and filter it
 
   
-   searchingg.addEventListener('input', () => searchStates(searchingg.value)); //function searchStates need to be change to "searchOdu"
+   search.addEventListener('input', () => searchStates(search.value)); //function searchStates need to be change to "searchOdu"
 
 //json configuration for the new autocomplete searchbox
 
