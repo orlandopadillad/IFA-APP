43d77b9ea3aca8ead027582d5a06eadd4035924d
orlandopadillad <71845994+orlandopadillad@users.noreply.github.com>
2021-01-27 02:15:48 -0500

    snippet creation

---
+++ assets/js/snippets/header-bar.js
@@ -0,0 +1,10 @@
+document.write(`<header id="header">
+<a href="Odu-Template.html" class="logo"><strong>IFA PERSONAL LIBRARY</strong> All my IFA information in one place.</a>
+<ul class="icons">
+    <li><a href="#" class="icon brands fa-twitter" style="display: none;" ><span class="label">Twitter</span></a></li>
+    <li><a href="#" class="icon brands fa-facebook-f" style="display: none;"><span class="label">Facebook</span></a></li>
+    <li><a href="#" class="icon brands fa-snapchat-ghost" style="display: none;"><span class="label">Snapchat</span></a></li>
+    <li><a href="#" class="icon brands fa-instagram" style="display: none;"><span class="label">Instagram</span></a></li>
+    <li><a href="#" class="icon brands fa-medium-m"style="display: none;"><span class="label">Medium</span></a></li>
+</ul>
+</header>`)
