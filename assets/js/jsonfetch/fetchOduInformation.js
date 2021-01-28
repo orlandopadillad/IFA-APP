eb77a71f8fbc818328f51ff4d7596527b2a849d7
orlandopadillad <71845994+orlandopadillad@users.noreply.github.com>
2021-01-28 02:33:49 -0500

    json

--- assets/js/jsonfetch/fetchOduInformation.js
+++ assets/js/jsonfetch/fetchOduInformation.js
@@ -1,27 +1,30 @@
+
 function fetchOduData (){
     var htmlOutPut = document.getElementById("oduData").innerHTML="";//delete the text from the previous execution
-    fetch (oduInfoAddress).then(response =>{ //url of the json
+    fetch (oduJSONAddress).then(response =>{ //url of the json
         console.log(response);//load if the json was loaded --- this can be deleted.
         if (!response.ok){
             throw Error ("ERROR");
         }
         return response.json();
     })
-    .then(datas =>{ //acces to load the info the name DATA --- can be change but need to be update on all the places.
-        console.log(datas.osaOgbe.ebbos);//retreieved  information information to the console, --- this can be deleted.
-        var htmlOutPut = datas.osaOgbe.ebbos.map(infoDisplay => { //route to retreieve the information --- can be change but need to be update on all the places.
+    .then(data =>{ //acces to load the info the name DATA --- can be change but need to be update on all the places.
+        console.log(data[oduForTheJSON]['ebbos']);//retrive  information information to the console, --- this can be deleted.
+          htmlOutPut = data[oduForTheJSON]['ebbos'].map(infoDisplay => { //route to retreieve the information --- can be change but need to be update on all the places.
            //below code "return" the HTML code
-            return `
-            <p>${infoDisplay.ENG}</p>
-            `;
+           return `
+           <b>ẹbọ  ${infoDisplay.id}</b><br><p>${infoDisplay.translation.SPA}</p>
+           `;
         })
         .join('');//transform the HTML in a string
         console.log(htmlOutPut) //log to grab all the info and put it into the HTML
         document
             .querySelector('#oduData') //id where to put the info
-            .insertAdjacentHTML("afterbegin" , htmlOutPut);// insert the text exactly after the id declared above
+            .insertAdjacentHTML("afterend" , htmlOutPut);// insert the text exactly after the id declared above
     }).catch(error => {
         console.log(error);
     });
 }
+
 fetchOduData ()
+
